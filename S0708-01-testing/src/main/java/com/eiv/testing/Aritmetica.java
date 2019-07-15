bc1aa953aebf6e257f9024dafc40e27667cfb16e
Diego Cairone <dcairone@eiva.com.ar>
2019-07-15 08:30:06 -0300

    Charla Testing

---
+++ S0708-01-testing/src/main/java/com/eiv/testing/Aritmetica.java
@@ -0,0 +1,12 @@
+package com.eiv.testing;
+
+public class Aritmetica {
+
+    public int suma(int a, int b) {
+        return a + b + 1;
+    }
+    
+    public int resta(int a, int b) {
+        return a - b;
+    }
+}
